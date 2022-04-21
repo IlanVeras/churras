ced9459b65e3644e146f4de66217b7d2cf4da879
Ilan Veras <91434030+IlanVeras@users.noreply.github.com>
2022-04-21 16:40:48 -0300

    Background-image
    problemas no gradiente

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let inputAdulto = document.getElementById('adultos')
 let inputCrianca = document.getElementById('criancas')
-let inputDuracao = document.getElementById('duracao')
+let duracao = document.getElementById('duracao').value
 let inputVegano = document.getElementById('veganos')
 
 let resultado = document.getElementById('resultado')
@@ -10,26 +10,27 @@ function calcular(){
     let adultos = inputAdulto.value
     let criancas = inputCrianca.value
     let veganos = inputVegano.value
-    let horas = inputDuracao.value
+    let horas = duracao
 
     let qtdTotalCarne = carnePP(horas) * adultos + (carnePP(horas)/2 * criancas)
     let qtdTotalCerveja = cervejaPP(horas) * adultos
     let qtdTotalBebida = bebidasBB(horas) * adultos + (bebidasBB(horas)/2 * criancas)
     let qtdTotalvegan = paoAlho(horas) * veganos
-    resultado.style.padding = '20px'
     if(adultos > 0){
         resultado.innerHTML = `<p>${qtdTotalCarne/1000} Kg de Carne 🥩</p>`
         resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 355)} latinhas de Cerveja 🍺</p>`
+        resultado.style.padding = '20px'
     }
 
     if(criancas > 0){
-        resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebida / 1000)} garrafinhas de 1 l 🥤</p>`
+         resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebida / 1000)} garrafinhas de 1 l 🥤</p>`
+         resultado.style.padding = '20px'
     }
 
     if(veganos > 0){
-        resultado.innerHTML += `<p>${Math.ceil(qtdTotalvegan)} unidades de pão de alho 🍞🧄</p>`
-    } else{
-        resultado.innerHTML = "Ninguém vai ao churrasco"
+         resultado.innerHTML += `<p>${Math.ceil(qtdTotalvegan)} unidades de pão de alho 🍞🧄</p>`
+         document.body.style.background = 'linear-gradient(yellow, green)'
+         //PROBLEMAS
     }
 }
 
